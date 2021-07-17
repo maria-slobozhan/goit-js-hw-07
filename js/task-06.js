82e4d2ea7e3654c12a21670551646dcd63181b30
maria-slobozhan <slobozhan.mi@gmail.com>
2021-07-17 23:23:14 +0300

    update 6 task

--- js/task-06.js
+++ js/task-06.js
@@ -4,8 +4,11 @@ console.log(valideInputLength);
 
 const validateInput = (event) => {
   if (event.target.value.length !== valideInputLength) {
-    validationInput.classList.add("invalid");
-  } else validationInput.classList.replace("invalid", "valid");
+    validationInput.classList.add("invalid") ||
+      validationInput.classList.replace("valid", "invalid");
+  } else
+    validationInput.classList.add("valid") ||
+      validationInput.classList.replace("invalid", "valid");
 };
 
 validationInput.addEventListener("blur", validateInput);
