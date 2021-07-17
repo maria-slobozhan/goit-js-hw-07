b083a3143129479ea0e6126f0068c8e772e5dfd3
maria-slobozhan <slobozhan.mi@gmail.com>
2021-07-17 14:44:20 +0300

    finish7tasks

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -5,11 +5,7 @@ const itemsList = document.querySelectorAll(".item ul");
 const countCategories = (element) => `В списке ${element.length} категории.`;
 console.log(countCategories(category));
 
-const categoryResult = (items) => {
-  items.forEach((item) =>
-    console.log(`Категория: ${item.firstElementChild.textContent}
+const categoryResult = category.forEach((item) =>
+  console.log(`Категория: ${item.firstElementChild.textContent}
 Количество элементов:${item.querySelectorAll("li").length} `)
-  );
-};
-
-console.log(categoryResult(category, itemsList));
+);
